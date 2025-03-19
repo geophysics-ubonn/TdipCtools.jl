aa8cb3a3b2a308e8fc390ab6297bf9a8cfaee266
Joost <hase@geo.uni-bonn.de>
2025-03-19 15:29:07 +0100

    convert files now takes tomo datasets

--- test/test_interface/create_files.jl
+++ test/test_interface/create_files.jl
@@ -14,6 +14,8 @@ d = d .+ noise_realization
 
 Cinv = ones(length(d)) .* std^-2
 
+### Manager
+
 # write timesteps
 open("test_interface/t.txt", "w") do io
     writedlm(io, timesteps)
@@ -33,4 +35,38 @@ freqs = 10.0 .^ (range(-2, 1, 100))
 # write freqs
 open("test_interface/freqs.txt", "w") do io
     writedlm(io, freqs)
+end
+
+# write R0
+open("test_interface/r0.txt", "w") do io
+    writedlm(io, Float64[2.0])
+end
+
+
+### TomoManager
+
+# write timesteps
+open("test_interface/tomo_t.txt", "w") do io
+    writedlm(io, cat(timesteps, timesteps, dims=2))
+end
+
+# write transient
+open("test_interface/tomo_d.txt", "w") do io
+    writedlm(io, cat(d, d, dims=2))
+end
+
+# write cinv
+open("test_interface/tomo_cinv.txt", "w") do io
+    writedlm(io, cat(Cinv, Cinv, dims=2))
+end
+
+freqs = 10.0 .^ (range(-2, 1, 100))
+# write freqs
+open("test_interface/tomo_freqs.txt", "w") do io
+    writedlm(io, freqs)
+end
+
+# write R0
+open("test_interface/tomo_r0.txt", "w") do io
+    writedlm(io, fill(2.0, 2))
 end
--- test/test_interface/test_interface.jl
+++ test/test_interface/test_interface.jl
@@ -4,6 +4,16 @@
         "test_interface/d.txt",
         "test_interface/cinv.txt",
         "test_interface/freqs.txt",
-        "test_interface/spectrum.txt"
+        "test_interface/spectrum.txt",
+        "test_interface/r0.txt"
+    )
+
+    convert_files(
+        "test_interface/tomo_t.txt",
+        "test_interface/tomo_d.txt",
+        "test_interface/tomo_cinv.txt",
+        "test_interface/tomo_freqs.txt",
+        "test_interface/tomo_spectrum.txt",
+        "test_interface/tomo_r0.txt"
     )
 end
